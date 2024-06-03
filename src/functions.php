7b467dcb496fe6e34c22a37f8d18f4e2afe4372b
AhJ <amirhosseinjafari8228@gmail.com>
2024-06-03 23:11:56 +0330

    add func

--- src/functions.php
+++ src/functions.php
@@ -1,1 +1,12 @@
 <?php declare(strict_types=1);
+
+namespace Amp\SQLite3;
+
+function connect(
+    string $filename,
+    int $flags = SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE,
+    string $encryptionKey = ''
+): SocketSQLite3Connection {
+    $config = new SQLite3Config($filename, $flags, $encryptionKey);
+    return SocketSQLite3Connection::connect($config);
+}
